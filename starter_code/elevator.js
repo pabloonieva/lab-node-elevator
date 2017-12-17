f26c8844ba7c62df0e6ba7ed860ef03b8153b3e5
pabloonieva <pablo@ironhack.com>
2017-12-17 17:06:08 +0100

    2nd commit. feat: floorUp/Down functions

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,25 +1,35 @@
-class Elevator {
+class Elevator{
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
   }
 
-  start() {
+  start(){
     var interval = setInterval(this.update(),1000);
   }
-  stop() {
+  stop(){
     clearInterval(interval);
   }
-  update() {
+  update(){
       this.log();
   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() {
+  floorUp(){
+    if(this.floor < this.MAXFLOOR){
+      this.floor += 1;
+    }
+  }
+  floorDown(){
+    if(this.floor > 0){
+      this.floor -= 1;
+    }
+  }
+  call(){
+
+  }
+  log(){
     console.log("Direction: " + this.requests + " | Floor: " + this.floor);
   }
 }
